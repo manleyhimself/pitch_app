a0936c7d4986f72e2375358f018304f385308389
gaw1990 <georgewambold@gmail.com>
2016-01-26 15:51:25 -0600

    Create the following working associations: User .likes, .likees, .inverse_likes and .inverse_likees.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,6 @@
 class Like < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :likee, class_name: "User"
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+
+  has_many :likes, class_name: "Like"
+  has_many :likees, through: :likes 
+  has_many :inverse_likes, class_name: "Like", foreign_key: "likee_id"
+  has_many :inverse_likees, through: :inverse_likes, source: :user
+  
 end
