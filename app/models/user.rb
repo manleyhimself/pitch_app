6141506db8049f65c5dec78c3e29a8bb9338e943
gaw1990 <georgewambold@gmail.com>
2016-01-26 16:16:24 -0600

    Refactor user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  has_many :likes, class_name: "Like"
+  has_many :likes
   has_many :likees, through: :likes 
   has_many :inverse_likes, class_name: "Like", foreign_key: "likee_id"
   has_many :inverse_likees, through: :inverse_likes, source: :user
