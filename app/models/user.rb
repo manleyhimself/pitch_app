5765861f2c679af0ccb8de1036c4a3379ec05948
manleyhimself <bcmanley1@gmail.com>
2016-01-26 21:45:17 -0500

    Adds notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,8 @@
 class User < ActiveRecord::Base
 
   has_many :likes
-  has_many :likees, through: :likes 
-  has_many :inverse_likes, class_name: "Like", foreign_key: "likee_id"
-  has_many :inverse_likees, through: :inverse_likes, source: :user
+  has_many :likees, through: :likes #users self has liked
+  has_many :inverse_likes, class_name: "Like", foreign_key: "likee_id" 
+  has_many :inverse_likees, through: :inverse_likes, source: :user #users who have liked self
   
 end
