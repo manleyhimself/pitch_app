3223ae6ff65bb3ee0d4235f042e8a735cf3b06a3
manleyhimself <bcmanley1@gmail.com>
2016-01-28 15:25:10 -0500

    Attempts to create custom has_many relationships through joins tables; having some issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,11 +18,36 @@
 #
 
 class User < ActiveRecord::Base
-
   has_many :likes
-  has_many :likees, through: :likes #users self has liked
-  has_many :inverse_likes, class_name: "Like", foreign_key: "likee_id" 
-  has_many :inverse_likees, through: :inverse_likes, source: :user #users who have liked self
 
+  # has_many :likees, through: :likes #users self has liked
+  # has_many :inverse_likes, class_name: "Like", foreign_key: "likee_id" 
+  # has_many :inverse_likees, through: :inverse_likes, source: :user #users who have liked selfrelq
+
+  def likes
+    Like.where('(user_id = ?) OR (likee_id = ? AND match = ?)', id, id, true)
+  end
+
+  def likees
+    User.joins(:likes)
+      .where('(likes.user_id = ?) OR (likes.likee_id = ? AND likes.match = ?)', id, id, true)
+
+      # TODO: problem is that when following like is found, Like(user_id: self.id, likee_id: other_user.id), 
+        # it is returning user through user_id rather than likee_id. Therefore, we are returning self.
+  end
+
+  def inverse_likes
+    Like.where('(likee_id = ?) OR (user_id = ? AND match = ?)', id, id, true)
+  end
+
+  def inverse_likees
+    User.joins(:likes)
+      .where('(likes.likee_id = ?) OR (likes.user_id = ? AND likes.match = ?)', id, id, true)
+  end
+
+  def matches
+    User.joins(:likes)
+      .where('(likes.user_id = ? AND likes.match = ?) OR (likes.likee_id = ? AND likes.match = ?)', id, true, id, true)
+  end
   
 end
