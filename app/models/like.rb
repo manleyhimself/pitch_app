5dc9c0059f664dcc45bc38d68735852dc406fcab
manleyhimself <bcmanley1@gmail.com>
2016-02-01 13:49:46 -0500

    Adds create_or_update method to likes model + tests

--- app/models/like.rb
+++ app/models/like.rb
@@ -18,6 +18,24 @@
 
 class Like < ActiveRecord::Base
 
-  # TODO: build custom associations with, similar to desigin in user model's
+  belongs_to :user
+  belongs_to :likee, class: User
+
+  # this method assumes that, elsewhere, we prevent users from liking the same user twice
+  # -OR- periodically clear the un-matched likes 
+  def self.create_or_update base_user_id: nil, target_user_id: nil
+    inverse_like = find_inverse_like(base_user_id: base_user_id, target_user_id: target_user_id)
+
+    if inverse_like.present?
+      inverse_like.update_attributes(match: true, match_time: Time.now)
+      inverse_like
+    else
+      create(user_id: base_user_id, likee_id: target_user_id)
+    end
+  end
+
+  def self.find_inverse_like base_user_id: nil, target_user_id: nil
+    where(user_id: target_user_id, likee_id: base_user_id).first
+  end
   
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -19,5 +19,36 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { FactoryGirl.create(:user) }
+
+  describe 'user liking' do
+    let!(:base_user) { FactoryGirl.create(:user) }
+    let!(:target_user) { FactoryGirl.create(:user) }
+
+    it 'creates a new like if the base user has not been liked by the target user' do
+      before_like_count = Like.count
+      Like.create_or_update(base_user_id: base_user.id, target_user_id: target_user.id)
+      expect(Like.count).to eq(before_like_count + 1)
+    end
+
+    it 'it does not create a new like if the base user has already been liked by the target user' do 
+      Like.create_or_update(base_user_id: target_user.id, target_user_id: base_user.id)
+      before_like_count = Like.count
+      Like.create_or_update(base_user_id: base_user.id, target_user_id: target_user.id)
+      expect(Like.count).to eq(before_like_count)
+    end
+
+    it 'returns a like with match set to false if the base user has not been liked by the target user' do
+      like = Like.create_or_update(base_user_id: base_user.id, target_user_id: target_user.id)
+      expect(like.match?).to be(false)
+    end
+
+    it 'returns a like with match set to true if the base user has been liked by the target user' do 
+      Like.create_or_update(base_user_id: target_user.id, target_user_id: base_user.id)
+      like = Like.create_or_update(base_user_id: base_user.id, target_user_id: target_user.id)
+      expect(like.match?).to be(true)
+    end
+
+  end
+  
 end
